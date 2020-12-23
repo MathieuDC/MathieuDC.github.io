884d8f691da001e9da7dc49142e14dc92e05a081
MathieuDC13 <mdecampou@gmail.com>
2020-12-23 12:24:04 +0100

    rm files

--- README.md
+++
@@ -1,1 +0,0 @@
-# MathieuDC.github.io
--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('scroll', () => {
-    if($(window).scrollTop() == 0){
-        document.getElementById('mdc-nav-container').classList.remove('is-scrolling')
-    }else{
-        document.getElementById('mdc-nav-container').classList.add('is-scrolling')
-    }
-});
